1f42f4fd912c8f7f48bd80e5a5e349f02568ee22
Idan <gigiidan@gmail.com>
2024-07-09 01:16:13 +0300

    Add class validation pipes

--- src/income/dto/createIncome.dto.ts
+++ src/income/dto/createIncome.dto.ts
@@ -1,17 +1,24 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { INCOME_TYPE } from '../income.schema';
+import { IsDate, IsEnum, IsNumberString, IsString } from 'class-validator';
 
 export class CreateIncomeDto {
+  @IsNumberString()
   @ApiProperty({ required: true })
   amount: string;
+  @IsNumberString()
   @ApiProperty()
   fundAmount: string;
   @ApiProperty()
+  @IsString()
   description: string;
   @ApiProperty({ required: true })
+  @IsEnum(INCOME_TYPE)
   incomeType: INCOME_TYPE;
   @ApiProperty({ required: true })
+  @IsString()
   source: string;
   @ApiProperty({ required: true })
+  @IsDate()
   transactionDate: Date;
 }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from './pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,7 @@ async function bootstrap() {
     .addTag('Auth')
     .build();
   app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe());
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   await app.listen(3000);
