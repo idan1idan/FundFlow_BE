48d603d69fbb338cb1c7e8a749e442cf11e2b81b
Idan <gigiidan@gmail.com>
2024-08-06 12:49:39 +0300

    remove comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { IncomeModule } from './income/income.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-// 'mongodb://localhost:27017/fundFlow'
 @Module({
   imports: [
     FundModule,
