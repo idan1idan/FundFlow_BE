6b9176beab341dec11237935841d96ccb7d530b3
Idan <gigiidan@gmail.com>
2024-08-06 12:49:21 +0300

    Add config to mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,19 @@ import { FundModule } from './fund/fund.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { IncomeModule } from './income/income.module';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+// 'mongodb://localhost:27017/fundFlow'
 @Module({
   imports: [
     FundModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/fundFlow'),
+    MongooseModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
+      inject: [ConfigService],
+    }),
     IncomeModule,
     AuthModule,
     ConfigModule.forRoot({
